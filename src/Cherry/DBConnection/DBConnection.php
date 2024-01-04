172ea8bb8fd7de268f28e0192e9b0e59b30808e0
Callum Langley <callumlangley9@gmail.com>
2024-01-04 20:07:48 +0000

    Adde declare(strict_types=1);

--- src/Cherry/DBConnection/DBConnection.php
+++ src/Cherry/DBConnection/DBConnection.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Cherry\DBConnection;
 
 use Cherry\DBConnection\Exception\DBConnectionException;
--- src/Cherry/Router/Router.php
+++ src/Cherry/Router/Router.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Cherry\Router;
 
 use Cherry\Router\RouterInterface;
